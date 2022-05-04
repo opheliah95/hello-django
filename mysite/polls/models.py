e9608aadb4513c431f6be05bead153a39e0a58f8
opheliah95 <opheliah95@gmail.com>
2022-05-04 22:23:40 +0100

    fixed database migration error

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,7 +5,7 @@ class Question(models.Model):
     # create some text -> stored in our db
     question_text: models.CharField = models.CharField(max_length=200)
     # publication date -> create a field like this in our db
-    pub_date: models.DateTimeField = models.DateTimeField(name="date published")
+    pub_date: models.DateTimeField = models.DateTimeField("date published")
 
 #  class for choice
 class Choice(models.Model):
