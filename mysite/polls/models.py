2d8cf5c0bfae7d617f78c71254b60574378d05f5
opheliah95 <opheliah95@gmail.com>
2022-05-04 17:03:28 +0100

    models: initial db model

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    # create some text -> stored in our db
+    question_text: models.CharField = models.CharField(max_length=200)
+    # publication date -> create a field like this in our db
+    pub_date: models.DateTimeField = models.DateTimeField(name="date published")
+
+#  class for choice
+class Choice(models.Model):
+    # using question as a foregion key to get selected q from db
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default = 0)
+
