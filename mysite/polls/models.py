3fd82858d9537345de896609718410f8704c7e3e
opheliah95 <opheliah95@gmail.com>
2022-05-08 22:36:23 +0100

    feat: added custom method

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+from time import timezone
 from django.db import models
+import datetime
 
 # Create your models here.
 class Question(models.Model):
@@ -11,14 +14,18 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    # check if question is published recently
+    def is_recent(self) -> bool:
+        return self.pub_date > timezone.now() - datetime.timedelta(days=1)
+
+
 #  class for choice
 class Choice(models.Model):
     # using question as a foregion key to get selected q from db
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default = 0)
+    votes = models.IntegerField(default=0)
+
     # return an easy to read choice type
     def __str__(self):
         return self.choice_text
-
-
