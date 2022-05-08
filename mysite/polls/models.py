daa7ee735469f186a35cec4f43508fd3a81a89e5
opheliah95 <opheliah95@gmail.com>
2022-05-08 22:32:00 +0100

    models: edit default __str__ method

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -7,10 +7,18 @@ class Question(models.Model):
     # publication date -> create a field like this in our db
     pub_date: models.DateTimeField = models.DateTimeField("date published")
 
+    # make return type more easy to read
+    def __str__(self):
+        return self.question_text
+
 #  class for choice
 class Choice(models.Model):
     # using question as a foregion key to get selected q from db
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default = 0)
+    # return an easy to read choice type
+    def __str__(self):
+        return self.choice_text
+
 
