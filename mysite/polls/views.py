d5741d6ce0ba413d12c28a6fa5caf47464c570d1
opheliah95 <opheliah95@gmail.com>
2022-05-11 00:25:00 +0100

    added sample view for detail page

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # return some basic rendering for index page
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+# view index for detail page
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
