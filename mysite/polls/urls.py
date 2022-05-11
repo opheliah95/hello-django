b7436d42dbd16efd3da0c053fef23f6ad2b0a471
opheliah95 <opheliah95@gmail.com>
2022-05-11 19:26:58 +0100

    feat: view for result

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name="detail")
+    path('<int:question_id>/', views.detail, name="detail"),
+    path('<int:question_id>/result', views.result, name="result")
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,3 +8,8 @@ def index(request):
 # view index for detail page
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
+
+# view for result page
+def result(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
