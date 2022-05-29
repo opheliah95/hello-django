2460362d3cdea839c7356fe11699079aeba5732d
opheliah95 <opheliah95@gmail.com>
2022-05-29 22:46:24 +0100

    feat: return individual questions

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,24 +2,35 @@ import os
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
+from django.shortcuts import render
+from django.http import Http404
 
 # return some basic rendering for index page
 def index(request):
-    latest_questions = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+    latest_questions = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
     context = {
-        'latest_questions': latest_questions,
+        "latest_questions": latest_questions,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
+
 
 # view index for detail page
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+        context = {"question": question}
+    except Question.DoesNotExist:
+        raise Http404(f"the question {question_id} does not exist")
+
+    return render(request, "polls/detail.html", context)
+
 
 # view for result page
 def result(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
+
 
 # the vote page
 def vote(request, question_id):
