a625f980e6b9f51a73ea042af6f576ba5c0d76fa
opheliah95 <opheliah95@gmail.com>
2022-05-30 12:50:06 +0100

    vote: added func when press vote it will direct you to the result

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,6 +6,6 @@ app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:question_id>/", views.detail, name="detail"),
-    path("<int:question_id>/result", views.result, name="result"),
+    path("<int:question_id>/results", views.result, name="results"),
     path("<int:question_id>/vote", views.vote, name="vote"),
 ]
