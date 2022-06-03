2bef7de26450f026f939dcde72bc7895999083f2
opheliah95 <opheliah95@gmail.com>
2022-06-03 18:51:29 +0100

    admin: added choices to be edible as well

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 # make question editable in adminstration window
 admin.site.register(Question)
+
+# we will register choice as edible as well
+admin.site.register(Choice)
