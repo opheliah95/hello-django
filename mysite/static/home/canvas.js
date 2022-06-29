2f47e5732e38cbd8ef90d4e35af8f3a7a11b27bc
opheliah95 <opheliah95@gmail.com>
2022-06-29 23:32:56 +0100

    feat: circle move in different directions

--- mysite/static/home/canvas.js
+++ mysite/static/home/canvas.js
@@ -18,44 +18,50 @@ class Circle {
 
         // params for offset
         this.dx = Math.random() * 10 + 10;
-        this.dx *= Math.floor(Math.random() * 2 == 1) ? 1 : -1
         this.dy = Math.random() * 10 + 10;
-        this.dy *= Math.floor(Math.random() * 2 == 1) ? 1 : -1
     }
 
-    // function to draw a single circle
-    draw() {
+     // function to draw a single circle
+     draw() {
         ctx.beginPath();
         ctx.fillStyle = this.c;
         ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
         ctx.fill();
     }
+
     // function to animate the circle
-    animate(secondsPassed = 1000) {
+    animate() {
         this.x += this.dx;
         this.y += this.dy;
         this.draw();
     }
+
+    // return seed value 
+    generate_seed(){
+        var seed = Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        this.dx *= seed;
+        this.dy *= seed;
+        console.log(f`hello: {seed}`)
+    }
 }
 
-// a class for creating bubble
-const bubbles = [];
-const balls_amount = 50;
+    // a class for creating bubble
+    const bubbles = [];
+    const balls_amount = 50;
 
-for (let i = 0; i < balls_amount; i++) {
+    for(let i = 0; i <balls_amount; i++) {
     let r = Math.floor(Math.random() * 30 + 20);
     let x = Math.random() * (canvas.width - 2 * r) + r;
     let y = Math.random() * (canvas.height - 2 * r) + r;
-    let c = 'red';
+    let c = 'blue';
     bubbles.push(new Circle(ctx, x, y, r, c));
 }
 
-function Update(timeStamp = 1000) {
-    var secondsPassed = (timeStamp - oldTimeStamp) / 1000;
-    var oldTimeStamp = timeStamp;
+function Update() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     for (let i = 0; i < balls_amount; i++) {
         bubbles[i].animate();
+        bubbles[i].generate_seed();
     }
 }
 
